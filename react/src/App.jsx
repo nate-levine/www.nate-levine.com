8afde66c72e106d3dc6da36bd423e7f5f5090d01
Nate Levine <n8levine11@gmail.com>
2022-10-12 22:26:18 -0400

    hopefully this works now

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default () => (
   <>
-    <p>This is to make sure React is working.</p>
+    <p>This is to make sure React is working. Hopefully it finally works.</p>
   </>
 );
