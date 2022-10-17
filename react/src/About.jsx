146f88611aabe47bf857da07b61286d92ecdad6e
Nate Levine <n8levine11@gmail.com>
2022-10-17 19:37:48 -0400

    reformatting

--- react/src/About.jsx
+++ react/src/About.jsx
@@ -5,7 +5,7 @@ let About = () => {
     return(
         <div>
             <NavigationBar />
-            <h1>WHAT'S UP?</h1>
+            <h1>WHAT'S UP</h1>
             <p>This site is under construction</p>
             <p>Check back in a week...</p>
         </div>
